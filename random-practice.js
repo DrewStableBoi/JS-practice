b7250a213f78540df72d3a0b8a5999b558ea6ecb
Drew Hemsley <drew@grow.com>
2019-05-16 18:25:23 -0600

    more practice

--- random-practice.js
+++ random-practice.js
@@ -15,3 +15,44 @@ var getInitials = function (string) {
 
 console.log(getInitials('Drew Hemsley'));
 
+//More random practice - May 16, 2019
+
+// write a function that takes in an array and a string. 
+// It should find the string in the array. If the string exists in the array
+// return true, else return false 
+// example:  
+
+function find(array, string) {
+    for(item of array) {
+        if (item === string) 
+        return true
+    }
+        return false;
+}
+
+//Another way to do it:
+
+// function find(arr, string) {
+//     for(let i = 0; i < arr.length; i++) {
+//       const item = arr[i];
+//       if (item === string) return true
+//     }
+//     return false
+// }
+
+//Another way to do this:
+
+// function find(arr, string) {
+//     if (arr.indexOf(string) !== -1) return true
+//     return false
+//   }
+
+const answer1 = find(['hey', 'hi', 'hello'], 'hi')
+
+console.log(answer1);
+
+const fastfood = ['tacobell', 'mcdonalds', 'panda express', 'arbys', 'shitface jenkins', 'arctic circle']
+
+const i = fastfood.indexOf('shitface jenkins');
+
+console.log(i);
